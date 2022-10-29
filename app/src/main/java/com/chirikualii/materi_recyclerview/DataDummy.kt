3e717815fc82db83a6c8f4f3d51aaef3b66ff8ca
DESKTOP-PQTPRE2\Lab-ERP-37 <rivanacahyani22@gmail.com>
2022-10-29 14:43:59 +0700

    PERTEMUAN 4

---
+++ app/src/main/java/com/chirikualii/materi_recyclerview/DataDummy.kt
@@ -0,0 +1,52 @@
+package com.chirikualii.materi_recyclerview
+
+object DataDummy  {
+
+    // TODO add data dummy
+    val ListProfile = listOf<Profile>(
+        Profile(
+            name = "Ratna",
+            phone = "032645411",
+            image = "https://avatars.githubusercontent.com/u/14894746?v=4"
+
+        ),
+        Profile(
+            name = "Ratna",
+            phone = "032645411",
+            image = "https://avatars.githubusercontent.com/u/14894746?v=4"
+
+        ),
+        Profile(
+            name = "Ratna",
+            phone = "032645411",
+            image = "https://avatars.githubusercontent.com/u/14894746?v=4"
+
+        ),
+        Profile(
+            name = "Ratna",
+            phone = "032645411",
+            image = "https://avatars.githubusercontent.com/u/14894746?v=4"
+
+        ),
+        Profile(
+            name = "Ratna",
+            phone = "032645411",
+            image = "https://avatars.githubusercontent.com/u/14894746?v=4"
+
+        ),
+        Profile(
+            name = "Ratna",
+            phone = "032645411",
+            image = "https://avatars.githubusercontent.com/u/14894746?v=4"
+
+        ),
+        Profile(
+            name = "Ratna",
+            phone = "032645411",
+            image = "https://avatars.githubusercontent.com/u/14894746?v=4"
+
+        ),
+
+
+    )
+}
---
+++ app/src/main/java/com/chirikualii/materi_recyclerview/ListProfileAdapter.kt
@@ -0,0 +1,8 @@
+package com.chirikualii.materi_recyclerview
+
+import androidx.recyclerview.widget.RecyclerView
+
+class ListProfileAdapter {
+    class holder : RecyclerView.ViewHolder(
+    )
+}
